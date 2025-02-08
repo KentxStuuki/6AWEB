06dbd1a07eb3fe4ce7cc754e90c4a594dcffc220
KentxStuuki <mequiambao@outlook.com>
2025-02-08 22:30:14 +0800

    Fixed files folders and mid lab 5 act done

---
+++ midterm/http-client-prg/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HttpclientService } from './httpclient.service';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+export class AppComponent {
+  title = 'clientprg';
+  httpusers: any;
+
+  constructor(private httpcclientList: HttpclientService) {
+    this.httpcclientList.getUsersRemotely().subscribe((data:any) => {
+      this.httpusers = data;
+    })
+  }
+}
