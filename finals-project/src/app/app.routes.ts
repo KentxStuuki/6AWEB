8fa2f3cfb208d2cda40440ff4cdc1c6a74d3dc29
Mark Erolle Quiambao <mequiambao@outlook.com>
2025-02-19 23:03:22 +0800

    Delete finals-project directory

--- finals-project/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
-import { TemplateDrivenComponent } from './template-driven/template-driven.component';
-import { ValidationDemoComponent } from './validation-demo/validation-demo.component';
-export const routes: Routes = [
-    { path: 'reactive-form', component: ReactiveFormComponent },
-    { path: 'template-driven', component: TemplateDrivenComponent },
-    { path: 'validation-demo', component: ValidationDemoComponent },
-];
